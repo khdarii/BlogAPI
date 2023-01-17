cce1200d71512c10edd2873f0b6b2390339ab35b
khdarii <kh.dariy145@gmail.com>
2023-01-18 00:12:19 +0200

    Blog API setup

--- posts/views.py
+++ posts/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from rest_framework import generics
+
+from .models import Post
+from .serializer import PostSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
